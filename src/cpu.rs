2a7f5bbc34d15a0feb0c51390c116d219689797c
PerfectlyInternal <alexnmartynov@1360.ca>
2025-09-05 15:22:43 -0400

    cpu docs

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,4 +1,9 @@
-// This is where code gets read and executed
+//! ConcordeVM's CPU.
+//!
+//! Provides a CPU struct that executes instructions in memory, according to a stack of instruction
+//! pointers.
+//!
+//! Instructions are stored as `Vec<Instruction>`s under symbols in memory. 
 
 use crate::instructions::{Instruction, execute_instruction};
 use crate::memory::*;
@@ -6,11 +11,22 @@ use crate::memory::*;
 use log::info;
 use std::vec::Vec;
 
+// `ExecutionPointer`s represent a location in memoty where code is being executed.
+//
+// Contains the symbol under which the instructions are stored, as well as the index of the
+// instruction currently being executed.
 struct ExecutionPointer {
     symbol: Symbol,
     index: usize,
 }
 
+// The `CPU` is where instruction reading and execution is handled.
+//
+// Contains `Memory`, as well as a stack of `ExecutionPointer`s.
+//
+// Currently, the stack does nothing of value, since we do not support branching or functions.
+// However, once those are implemented, the stack will be used to keep track of the return location
+// for every layer of function calls or branches.
 #[allow(clippy::upper_case_acronyms)]
 pub struct CPU {
     memory: Memory,
@@ -18,6 +34,7 @@ pub struct CPU {
 }
 
 impl CPU {
+    // Create a new `CPU`. Initializes both the memory and stack to be empty.
     pub fn new() -> CPU {
         CPU {
             memory: Memory::new(),
@@ -25,8 +42,12 @@ impl CPU {
         }
     }
 
-    // Complete one CPU cycle. This executes one instruction, and returns false iff the stack is
-    // empty and there is nothing more to execute
+    // Complete one CPU cycle. Returns false iff the stack is empty. Returns an error if something
+    // goes wrong during execution. Returns true otherwise.
+    //
+    // One CPU cycle does not necessarily map to one instruction, as a CPU cycle is used every time
+    // we pop an `ExecutionPointer` off of the stack when we are done executing those
+    // instructions. However, excluding that, each cycle executes one instruction.
     pub fn cycle(&mut self) -> Result<bool, String> {
         if let Some(exec_pointer) = self.stack.last_mut() {
             info!("Currently executing code at symbol [{}], index {}", exec_pointer.symbol.0, exec_pointer.index);
@@ -48,6 +69,7 @@ impl CPU {
         }
     }
 
+    // Load instructions into memory and add an `ExecutionPointer` for them to the stack.
     pub fn load_instructions(&mut self, instructions: &Vec<Instruction>, symbol: &Symbol) {
         self.memory.write(symbol, Data::new(instructions));
         self.stack.push(ExecutionPointer { symbol: symbol.clone(), index: 0 });
