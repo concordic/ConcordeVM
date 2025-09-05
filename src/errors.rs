9b2a982b9e14a3f38335434576437d7d7c7e4366
PerfectlyInternal <alexnmartynov@1360.ca>
2025-09-05 15:39:22 -0400

    error docs

--- src/errors.rs
+++ src/errors.rs
@@ -1,4 +1,6 @@
-// Output an error message to log and return an Err with the same message
+//! A utility that provides a useful error-handling macro.
+
+// Formats the inputs, logs the result, and returns an error with the same message. 
 #[macro_export]
 macro_rules! log_and_return_err {
     ($($t:tt)*) => {
